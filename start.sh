d022da5536398e78cfc0655ee19bfcd897447439
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-09-17 09:15:32 +0300

    new changes

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
- export MOVIE_API_KEY='14418ad36c8314366c84b2c215b37c89'
+    export MOVIE_API_KEY='14418ad36c8314366c84b2c215b37c89'
 export SECRET_KEY='os.urandom(32)'
+export MAIL_USERNAME='calebkimutai97@gmail.com'
+export MAIL_PASSWORD='Caleb254'
 
 python3.8 manage.py server
